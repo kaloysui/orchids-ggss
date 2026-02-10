a4db7472e8c0405649de48ec210ca579c606dd0f
kaloysui <ysc22tdtnk@privaterelay.appleid.com>
2026-02-10 15:11:34 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,14 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="antialiased">
-        <Script
-          id="orchids-browser-logs"
-          src="https://slelguoygbfzlpylpxfs.supabase.co/storage/v1/object/public/scripts/orchids-browser-logs.js"
-          strategy="afterInteractive"
-          data-orchids-project-id="565063ed-aab9-45a0-9034-de827a0f6dae"
-        />
 
-    
         <ErrorReporter />
     
 
